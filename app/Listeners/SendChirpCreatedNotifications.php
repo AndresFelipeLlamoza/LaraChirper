e9df056d2bd8faf1c4991f2167178027f4f41fc3
andresfelipellamoza <llamozafelipe@gmail.com>
2023-08-30 13:48:27 -0500

    Proyecto de laravel, mensajes y correos automaticos

---
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\chirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendChirpCreatedNotifications implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(chirpCreated $event): void
+    {
+        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user){
+            $user->notify(new NewChirp($event->chirp));
+        }
+    }
+}
